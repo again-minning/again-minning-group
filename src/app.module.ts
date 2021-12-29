1ecc27bd8de8eedf0b1614a1d831a59266d0dac6
solchan98 <dev.sol0427@gmail.com>
2021-12-29 09:59:28 +0900

    feat: #3 CustomExceptionFilter 작성

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,18 @@ import { GroupModule } from './group/group.module';
 import { MyGroupModule } from './my-group/my-group.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import * as ormConfig from '../ormconfig';
+import { APP_FILTER } from '@nestjs/core';
+import { CustomExceptionFilter } from './common/exception/custom.exception.filter';
 
 @Module({
   imports: [TypeOrmModule.forRoot(ormConfig), GroupModule, MyGroupModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_FILTER,
+      useClass: CustomExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
