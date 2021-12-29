4b6c1489811c2850902d8decdb2dd3e99189ee50
solchan98 <dev.sol0427@gmail.com>
2021-12-29 10:42:22 +0900

    feat: #3 그룹 상제 조회 테스트(unit, e2e) 작성 및 ToDoList.md Update

--- test/e2e/group.e2e-spec.ts
+++ test/e2e/group.e2e-spec.ts
@@ -2,7 +2,11 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../../src/app.module';
-import { ALL_GROUP_LIST, HEALTH_GROUP_LIST } from '../template/group.template';
+import {
+  ALL_GROUP_LIST,
+  GROUP_DETAIL,
+  HEALTH_GROUP_LIST,
+} from '../template/group.template';
 
 describe('GroupController (e2e)', () => {
   let app: INestApplication;
@@ -19,17 +23,30 @@ describe('GroupController (e2e)', () => {
     await app.close();
   });
 
-  it('/api/v1/group/0 (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/api/v1/group/0')
-      .expect(200)
-      .expect(ALL_GROUP_LIST);
+  describe('그룹_전체_조회_e2e', () => {
+    it('/api/v1/group/0 (GET)', () => {
+      return request(app.getHttpServer())
+        .get('/api/v1/group/0')
+        .expect(200)
+        .expect(ALL_GROUP_LIST);
+    });
   });
 
-  it('/api/v1/group/0 (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/api/v1/group/3')
-      .expect(200)
-      .expect(HEALTH_GROUP_LIST);
+  describe('건강_그룹_전체_조회_e2e', () => {
+    it('/api/v1/group/3 (GET)', () => {
+      return request(app.getHttpServer())
+        .get('/api/v1/group/3')
+        .expect(200)
+        .expect(HEALTH_GROUP_LIST);
+    });
+  });
+
+  describe('그룹_상세_조회_e2e', () => {
+    it('/api/v1/group/detail/1 (GET)', () => {
+      return request(app.getHttpServer())
+        .get('/api/v1/group/detail/1')
+        .expect(200)
+        .expect(GROUP_DETAIL);
+    });
   });
 });
--- test/service/group.service.spec.ts
+++ test/service/group.service.spec.ts
@@ -2,7 +2,12 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { GroupService } from '../../src/group/service/group.service';
 import { getRepositoryToken } from '@nestjs/typeorm';
 import { GroupRepository } from '../../src/group/repository/group.repository';
-import { groupAllList, groupListByHealth } from '../template/group.template';
+import {
+  GROUP_DETAIL,
+  groupAllList,
+  groupListByHealth,
+} from '../template/group.template';
+import { NotFoundException } from '@nestjs/common';
 
 describe('GroupService', () => {
   let service: GroupService;
@@ -11,6 +16,10 @@ describe('GroupService', () => {
   const mockRepository = {
     findAll: jest.fn().mockResolvedValue(groupAllList),
     findAllByCategory: jest.fn().mockResolvedValue(groupListByHealth),
+    findById: jest
+      .fn()
+      .mockResolvedValueOnce(GROUP_DETAIL)
+      .mockResolvedValueOnce(null),
   };
 
   beforeEach(async () => {
@@ -41,6 +50,24 @@ describe('GroupService', () => {
       const result = await service.getGroupList(3);
       expect(result.data.length).toEqual(1);
       expect(result.data[0].title).toEqual('러닝하기');
+    });
+  });
+
+  describe('그룹_상세_조회', () => {
+    it('group_id가 1번인 그룹을 조회한다.', async () => {
+      jest.spyOn(repository, 'findById');
+      const result = await service.getGroupDetail(1);
+      expect(result.title).toEqual('하루 한 잔 물 마시기');
+      expect(result.imageList.length).toEqual(2);
+    });
+  });
+
+  describe('없은_그룹_조회', () => {
+    it('없는 그룹을 조회하고 NotFoundException이 발생해야 한다.', async () => {
+      jest.spyOn(repository, 'findById');
+      await expect(service.getGroupDetail(0)).rejects.toThrow(
+        NotFoundException,
+      );
     });
   });
 });
