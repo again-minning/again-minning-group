ca9b4b1bb58380ad2a27fd576e8b24a5c4cdd945
solchan98 <dev.sol0427@gmail.com>
2022-01-01 05:00:14 +0900

    feat: #5 .env 설정(dev, test 구분)

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,9 +7,21 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import * as ormConfig from '../ormconfig';
 import { APP_FILTER } from '@nestjs/core';
 import { CustomExceptionFilter } from './common/exception/custom.exception.filter';
+import { ConfigModule } from '@nestjs/config';
+import * as path from 'path';
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ormConfig), GroupModule, MyGroupModule],
+  imports: [
+    TypeOrmModule.forRoot(ormConfig),
+    GroupModule,
+    MyGroupModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: path.resolve(
+        process.env.NODE_ENV === 'dev' ? '.env.dev' : '.env.test',
+      ),
+    }),
+  ],
   controllers: [AppController],
   providers: [
     AppService,
